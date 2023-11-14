2911a512cb39cf5a460b71ef3aa4483ca71e6216
Maaz Hassan <h.maaz809@hotmail.com>
2023-11-14 05:54:59 -0700

    Init commit

---
+++ Assets/PointCloudContainer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PointCloudContainer : MonoBehaviour {
+    private int counter = 0;
+    public GameObject keyPrefab;
+    public GameObject chairPrefab;
+    private GameObject container;
+
+    // Start is called before the first frame update
+    void Start() {
+        Application.targetFrameRate = 30;
+        container = GameObject.Find("PointCloudContainer");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (counter % 2 == 0) {
+            Destroy(container.transform.GetChild(0).GameObject());
+            GameObject newChild;
+            if (counter % 4 == 0) {
+                newChild = Instantiate(keyPrefab) as GameObject;
+            }
+            else {
+                newChild = Instantiate(chairPrefab) as GameObject;
+            }
+            newChild.transform.parent = container.transform;
+        }
+        counter++;
+    }
+}
